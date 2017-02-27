bc5fe8be59342d6352842b7c03e40a676eb9f4d7
Jeremy Brown <jeremy@zadarastorage.com>
2017-02-27 08:41:32 -0800

    Add sanity checks for ensuring metering db exists and docker is installed and reachable

--- zadara_metering/run.sh
+++ zadara_metering/run.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# If docker isn't responding to info command, or if the binary is missing
+# then notify and exit
+docker info > /dev/null 2>&1
+
+if [ $? -ne 0 ]; then
+  echo "Docker isn't installed or isn't running (e.g. Docker for Mac not launched).  Exiting."
+  exit 1
+fi
+
 CreateInfluxDB() {
   echo "Creating Influx database..."
   curl -s -X POST 'http://localhost:8086/query' --data-urlencode "q=CREATE DATABASE VPSA1"
@@ -35,7 +44,12 @@ AddDashboard() {
 
 ExtractAllMeteringFiles() {
   echo "Unzipping metering information..."
-  for meteringpackage in $(ls -1 metering*.zip); do
+  dirlist=$(ls -1 metering*.zip)
+  if [ $? -ne 0 ]; then
+    echo "Cannot locate any metering databases in this directory.  Please copy the database here then try again."
+    exit 1
+  fi
+  for meteringpackage in ${dirlist}; do
     targetfile=$(echo ${meteringpackage} | sed -e 's#^metering_\(.*\)_\(20[0-9]\)\(.*\).zip#\2\3#g' )
     targetfolder=$(echo ${meteringpackage} | sed -e 's#^metering_\(.*\)_'${targetfile}'.zip#\1#g' )
     echo "  Extracting ${meteringpackage} to ${targetfolder}/${targetfile}..."
