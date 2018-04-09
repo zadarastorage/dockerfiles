7154a6bca5701cd654cadb10f7389692dc2030fc
Eric <eric@zadarastorage.com>
2018-04-09 15:19:04 -0700

    service-cron: Add cron_dir env variable

--- service/ssh/alpine/start-cron.sh
+++ service/ssh/alpine/start-cron.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 if [ "${CRON_SERVICE}" == "enabled" ]; then
-	/usr/sbin/crond -f
+	if [ -n "${CRON_DIR}" ] && [ -d "${CRON_DIR}" ]; then
+		/usr/sbin/crond -f -C "${CRON_DIR}"
+	else
+		/usr/sbin/crond -f
+	fi
 else
 	while :; do
 		sleep 60m
