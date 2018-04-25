a75a7fce9b778d01d91af71d8aebc8ae4b50326f
Eric <eric@zadarastorage.com>
2018-04-25 15:24:57 -0700

    tools-autoexpand: Adjusting planned cron logic

--- tools/autoexpand/alpine/runonce-setCron.sh
+++ tools/autoexpand/alpine/runonce-setCron.sh
@@ -1,8 +1,14 @@
 #!/bin/ash
 # TODO: Support configurable cron schedules, for now every 10 minutes
 if [ -n "${CRON_DIR}" -a -d "${CRON_DIR}" ]; then
-	echo "*/10	*	*	*	*	/scripts/autoexpand.sh" > "${CRON_DIR}/autoexpand"
+	echo "*/5	*	*	*	*	/scripts/autoexpand.sh" > "${CRON_DIR}/autoexpand"
 else
 #	echo "*/10	*	*	*	*	/scripts/autoexpand.sh" > "/var/spool/cron/crontabs/expand"
-	ln -s /scripts/autoexpand.sh /etc/periodic/15min/autoexpand
+	if [ ! -d /etc/periodic/5min ]; then
+		mkdir /etc/periodic/5min
+	fi
+	if grep -q '/etc/periodic/5min' /var/spool/cron/crontabs/root; then
+		echo "*/5	*	*	*	*	run-parts /etc/periodic/5min" >> /var/spool/cron/crontabs/root
+	fi
+	ln -s /scripts/autoexpand.sh /etc/periodic/5min/autoexpand
 fi
