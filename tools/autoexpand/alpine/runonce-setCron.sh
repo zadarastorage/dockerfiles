03c897a519253e9d3653df1c4ee037992549d0b6
Eric <eric@zadarastorage.com>
2018-04-26 12:12:10 -0700

    tools-autoexpand: Accidentally inverted an if statement

--- tools/autoexpand/alpine/runonce-setCron.sh
+++ tools/autoexpand/alpine/runonce-setCron.sh
@@ -7,7 +7,7 @@ else
 	if [ ! -d /etc/periodic/5min ]; then
 		mkdir /etc/periodic/5min
 	fi
-	if grep -q '/etc/periodic/5min' /var/spool/cron/crontabs/root; then
+	if ! grep -q '/etc/periodic/5min' /var/spool/cron/crontabs/root; then
 		echo "*/5	*	*	*	*	run-parts /etc/periodic/5min" >> /var/spool/cron/crontabs/root
 	fi
 	ln -s /scripts/autoexpand.sh /etc/periodic/5min/autoexpand
