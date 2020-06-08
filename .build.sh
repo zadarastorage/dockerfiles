976796323c00f86a2d80f20cf75357b896ce76b4
eric-zadara <eric@zadarastorage.com>
2020-06-08 13:22:59 -0700

    added iperf, shifting to dynamically building multiple containers per job

---
+++ .build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Check necessary env vars
+EXIT=0
+for var in 'DOCKER_ORG' 'CONTAINER'; do
+	if [[ -z "${!var}" ]]; then
+		echo "[ERROR]: ${var} was not defined"
+		EXIT=1
+	fi
+done
+if [[ ${EXIT} -eq 1 ]]; then
+	exit 1
+fi
+OPWD=$(pwd)
+
+# Construct docker-compose build file from dockerfiles
+cd ${CONTAINER}
+COMPOSE='{"version": "3"}'
+for dockerfile in $(ls -1 Dockerfile.* | sort -V); do
+	TAG=${dockerfile#*.}
+	COMPOSE=$(echo ${COMPOSE} | jq --arg service "${TAG}" --arg dockerfile "${dockerfile}" --arg tag "${TAG}" --arg image "${DOCKER_ORG}/${CONTAINER}:${TAG}" '.services[$service] = {build: {context: ".", dockerfile: $dockerfile}, image: $image}')
+	if [[ -n "${LATEST}" && "${LATEST}" == "${TAG}" ]]; then
+		COMPOSE=$(echo ${COMPOSE} | jq --arg service "latest" --arg dockerfile "${dockerfile}" --arg tag "${TAG}" --arg image "${DOCKER_ORG}/${CONTAINER}:latest" '.services[$service] = {build: {context: ".", dockerfile: $dockerfile}, image: $image}')
+	fi
+done
+echo ${COMPOSE} | yq -y '.' > compose-build.yml
+cat compose-build.yml
+
+# Build all documented containers
+docker-compose -f compose-build.yml build
+# Push all documented containers
+docker-compose -f compose-build.yml push
+
+rm compose-build.yml
+cd ${OPWD}
