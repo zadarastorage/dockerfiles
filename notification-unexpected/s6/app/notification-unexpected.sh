dd5dca476515bc0c6285c6a8c54d2b3d7fc3050f
eric-zadara <eric@zadarastorage.com>
2020-08-28 15:29:41 -0700

    notification-unexpected: fix cron symlink, minor script re-org

--- notification-unexpected/s6/app/notification-unexpected.sh
+++ notification-unexpected/s6/app/notification-unexpected.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
+date -u > /lastrun
+
+# Functions
+function _log {
+	(>&2 echo "$(date +'%Y-%m-%d %H:%M:%S') ${0}: ${@}")
+}
+
+# Function to validate "empty" results from jq, possibly due to missing data
+function isEmpty {
+	if [[ -n "${@}" && "${@}" != "null" ]]; then
+		return 1
+	fi
+	return 0
+}
+
 if [[ -z "${VPSA_TOKEN}" ]]; then
-	echo "[$0] VPSA_TOKEN was undefined."
+	_log "VPSA_TOKEN was undefined."
 	exit 1
 fi
 
@@ -12,7 +27,6 @@ flock -n 200 || exit 1
 pid=$$
 echo $pid 1>&200
 
-date -u > /lastrun
 
 # Defaults and paths
 vcli=/app/vpsa_curl.sh
@@ -23,15 +37,7 @@ DATA_LIMIT=10
 CUTOFF_UNIX=$(date -d "-6 hours" +%s)
 SEARCH="Unexpected VC"
 
-# Function to validate "empty" results from jq, possibly due to missing data
-function isEmpty {
-	if [[ -n "${@}" && "${@}" != "null" ]]; then
-		return 1
-	fi
-	return 0
-}
-
-# Walk backwards from most recent active controller log
+# Walk backwards from most recent controller logs
 ## Check for "setting to active role was initiated."
 ## Check for SEARCH value
 CONT=1
@@ -51,16 +57,20 @@ while [[ ${CONT} -eq 1 ]]; do
 	# Time that controller begun "activation" process
 	if isEmpty "${ACTIVE_MARK}"; then
 		ACTIVE_MARK=$(echo ${RESULTS} | ${jq_raw} --arg needle "setting to active role was initiated." '[ .[] | select(.msg_title | contains($needle)) ][0].msg_id')
-		ACTIVE_SOURCE=$(echo ${RESULTS} | ${jq_raw} --arg needle "setting to active role was initiated." '[ .[] | select(.msg_title | contains($needle)) ][0].msg_attributes[] | select(.key=="controller").value')
+		if ! isEmpty "${ACTIVE_MARK}"; then
+			ACTIVE_SOURCE=$(echo ${RESULTS} | ${jq_raw} --arg msg_id "${ACTIVE_MARK}" '.[] | select(.msg_id==$msg_id).msg_attributes[] | select(.key=="controller").value')
+		fi
 	fi
 	# Time that search string occurred
 	if isEmpty "${UNEXPECTED_MARK}"; then
 		UNEXPECTED_MARK=$(echo ${RESULTS} | ${jq_raw} --arg needle "${SEARCH}" '[ .[] | select(.msg_title | contains($needle)) ][0].msg_id')
-		UNEXPECTED_SOURCE=$(echo ${RESULTS} | ${jq_raw} --arg needle "${SEARCH}" '[ .[] | select(.msg_title | contains($needle)) ][0].msg_attributes[] | select(.key=="controller").value')
-		UNEXPECTED_MSG=$(echo ${RESULTS} | ${jq_raw} --arg needle "${SEARCH}" '[ .[] | select(.msg_title | contains($needle)) ][0].msg_title')
-		UNEXPECTED_TIME=$(echo ${RESULTS} | ${jq_raw} --arg needle "${SEARCH}" '[ .[] | select(.msg_title | contains($needle)) ][0].msg_time')
-		if ! isEmpty "${UNEXPECTED_TIME}"; then
-			UNEXPECTED_UNIX=$(date -d "${UNEXPECTED_TIME}" +%s)
+		if ! isEmpty "${UNEXPECTED_MARK}"; then
+			UNEXPECTED_SOURCE=$(echo ${RESULTS} | ${jq_raw} --arg msg_id "${UNEXPECTED_MARK}" '.[] | select(.msg_id==$msg_id).msg_attributes[] | select(.key=="controller").value')
+			UNEXPECTED_MSG=$(echo ${RESULTS} | ${jq_raw} --arg msg_id "${UNEXPECTED_MARK}" '.[] | select(.msg_id==$msg_id).msg_title')
+			UNEXPECTED_TIME=$(echo ${RESULTS} | ${jq_raw} --arg msg_id "${UNEXPECTED_MARK}" '.[] | select(.msg_id==$msg_id).msg_time')
+			if ! isEmpty "${UNEXPECTED_TIME}"; then
+				UNEXPECTED_UNIX=$(date -d "${UNEXPECTED_TIME}" +%s)
+			fi
 		fi
 	fi
 	END_TIME=$(echo ${RESULTS} | ${jq_raw} '.[-1].msg_time')
@@ -78,8 +88,8 @@ while [[ ${CONT} -eq 1 ]]; do
 	## Current page > 1 and results are less than DATA_LIMIT
 	if [[
 		( ${ACTIVE_MARK} -gt 0 && ${UNEXPECTED_MARK} -gt 0 ) ||
-			( ${END_UNIX} -lt ${CUTOFF_UNIX} ) ||
-			( ${COUNT} -gt 1 && ${RESULTS_SIZE} -lt ${DATA_LIMIT} )
+			( ${COUNT} -gt 1 && ${RESULTS_SIZE} -lt ${DATA_LIMIT} ) ||
+			( ${END_UNIX} -lt ${CUTOFF_UNIX} )
 	]]; then
 		CONT=0
 	fi
@@ -90,13 +100,13 @@ done
 ## unexpected event predates current activation time
 ## unexpected event is older than cutoff
 ## Current activation is from different controller than unexpected event
-if \
-	isEmpty "${ACTIVE_MARK}" || \
-	isEmpty "${UNEXPECTED_MARK}" || \
-	isEmpty "${ACTIVE_SOURCE}" || \
-	isEmpty "${UNEXPECTED_SOURCE}" || \
-	[[ ${UNEXPECTED_MARK} -lt ${ACTIVE_MARK} ]] || \
-	[[ ${UNEXPECTED_UNIX} -lt ${CUTOFF_UNIX} ]] || \
+if 
+	isEmpty "${ACTIVE_MARK}" ||
+	isEmpty "${UNEXPECTED_MARK}" ||
+	isEmpty "${ACTIVE_SOURCE}" ||
+	isEmpty "${UNEXPECTED_SOURCE}" ||
+	[[ ${UNEXPECTED_MARK} -lt ${ACTIVE_MARK} ]] ||
+	[[ ${UNEXPECTED_UNIX} -lt ${CUTOFF_UNIX} ]] ||
 	[[ "${ACTIVE_SOURCE}" != "${UNEXPECTED_SOURCE}" ]]; then
 	exit 0
 fi
@@ -107,7 +117,7 @@ if [[ -e /unexpected && $(cat /unexpected) -ge ${UNEXPECTED_MARK} ]]; then
 fi
 
 # If we've gotten this far, generate ticket
-echo "Generating ticket"
+_log "Generating ticket, unexpected event was detected."
 TICKET_ID=$(echo "${UNEXPECTED_MSG}" | sed 's@.* ticket \([0-9]*\): .*@\1@g' )
 ${ticket} --template "${ticket_template}" --title "Unexpected event was detected in ticket #${TICKET_ID}" --replace TICKET_ID "${TICKET_ID}" --replace MESSAGE "${UNEXPECTED_MSG}" --replace TIME "$(date -d "@${UNEXPECTED_UNIX}" -u)"
 if [[ $? -eq 0 ]]; then
